e5410f48953082ceb8aad83db21f3122ee35721c
DaoThuPro <Thu86065@st.vimaru.edu.vn>
2023-03-01 10:19:05 +0700

    Thuc hanh

--- ThucHanh5/Cau7.dart
+++ ThucHanh5/Cau7.dart
@@ -4,15 +4,18 @@ void main() {
   // open file
   File file = File("students.csv");
   // write to file
-  file.writeAsStringSync('Name,Phone\n');
+  file.writeAsStringSync('Name, Address, phone\n');
   for (int i = 0; i < 3; i++) {
     // user input name
     stdout.write("Enter name of student ${i + 1}: ");
     String? name = stdin.readLineSync();
+    stdout.write("Enter address of student ${i + 1}: ");
+    //user input address
+    String? address = stdin.readLineSync();
     stdout.write("Enter phone of student ${i + 1}: ");
     // user input phone
     String? phone = stdin.readLineSync();
-    file.writeAsStringSync('$name,$phone\n', mode: FileMode.append);
+    file.writeAsStringSync('$name,$address,$phone\n', mode: FileMode.append);
   }
   // read file
   String contents = file.readAsStringSync();
